26b1901f92450f1c62cb5e72d76ea48dca3f8549
Tripti Saini <triptisaini123@gmail.com>
2020-09-07 00:58:22 +0530

    Updates

---
+++ precache-manifest.c8afac5ae8c0d12a2628a990a5de235f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb4c1fb75ca1cd6900003439bcdcf77",
+    "url": "/search-module/index.html"
+  },
+  {
+    "revision": "f8c67bd65cc2867ad45a",
+    "url": "/search-module/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "de725ce8b114c2503a25",
+    "url": "/search-module/static/css/main.68a3a7db.chunk.css"
+  },
+  {
+    "revision": "f8c67bd65cc2867ad45a",
+    "url": "/search-module/static/js/2.1601173d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/search-module/static/js/2.1601173d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de725ce8b114c2503a25",
+    "url": "/search-module/static/js/main.d9c114a6.chunk.js"
+  },
+  {
+    "revision": "b88a76b735b55a50305c",
+    "url": "/search-module/static/js/runtime-main.69d513f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/search-module/static/media/logo.5d5d9eef.svg"
+  }
+]);
